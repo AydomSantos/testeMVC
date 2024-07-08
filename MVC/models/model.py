57bddfa18bc567d8d89ecc8ce530398b5d45e9b7
AydomSantos <101830296+AydomSantos@users.noreply.github.com>
2024-07-08 15:07:28 -0300

    Organizando testando SOLID

--- MVC/models/model.py
+++ MVC/models/model.py
@@ -1,73 +1,70 @@
 import sqlite3
+from typing import Optional
 
 class Pessoa():
-    def __init__(self, nome, email, telefone, senha, senha_hash, id = None):
+    def __init__(self, nome: str, email: str, telefone: str, senha: str,  id: Optional[int] = None):
         self.id = id
         self.nome = nome
         self.email = email
         self.telefone = telefone
         self.senha = senha
-        self.senha_hash = senha_hash
-        
-    def cadastrar(self):
+      
+class PessoaRepository:    
+    def cadastrar(self, pessoa: Pessoa) -> None:
         conn = sqlite3.connect("conversor.db")
         cursor = conn.cursor()
-        cursor.execute('insert into usuarios (nome, email, telefone, senha) values(?, ?, ?, ?)',(self.nome, self.email, self.telefone, self.senha))
+        cursor.execute('INSERT INTO usuarios (nome, email, telefone, senha) VALUES (?, ?, ?, ?)', (pessoa.nome, pessoa.email, pessoa.telefone, pessoa.senha))
         conn.commit()
-        cursor.close()
-        conn.close() 
-         
-    def editar(self):
-        if self.id is None:
+        
+        
+    def editar(self, pessoa: Pessoa) -> None:
+        conn = sqlite3.connect("conversor.db")
+        cursor = conn.cursor()
+        if pessoa.id is None:
             raise ValueError("ID do usuário não especificado.")
         
-        conn = sqlite3.connect("conversor.db")
-        cursor = conn.cursor()
         cursor.execute("""
         UPDATE usuarios
         SET nome = ?, email = ?, telefone = ?, senha = ?
         WHERE id = ?
-        """, (self.nome, self.email, self.telefone, self.senha, self.id))
+        """,(pessoa.nome, pessoa.email, pessoa.telefone, pessoa.senha))
         conn.commit()
-        cursor.close()
-        conn.close()  
         
-    def excluir(self):
-        if self.id is None:
-            raise ValueError("ID do usuário não especificado.")
-        
+    
+    def excluir(self, pessoa: Pessoa) -> None:
         conn = sqlite3.connect("conversor.db")
         cursor = conn.cursor()
-        cursor.execute('''
-        'DELETE FROM usuarios WHERE id = ?'
-        ''',(self.id,))
+        if pessoa.id is None:
+            raise ValueError("ID do usuário não especificado.")
+        
+        cursor.execute("""
+          DELETE FROM usuarios WHERE id = ?', (pessoa.id,)             
+        """)
         conn.commit()
-        cursor.close()
-        conn.close()
+        
     
-    def buscar_pelo_email(self, email):
+    def pesquisar_por_email(self, email: str) -> Optional[Pessoa]:
         try:
-            conn = sqlite3.connect('conversor.db')  
-            cursor = conn.cursor()       
+            conn = sqlite3.connect('conversor.db')
+            cursor = conn.cursor()
             
-            cursor.execute('SELECT id, nome, email, senha_hash FROM pessoas WHERE email = ?', (email,)) 
+            cursor.execute('SELECT id, nome, email, telefone, senha, senha_hash FROM usuarios WHERE email = ?', (email,))
             result = cursor.fetchone()
             
             if result:
                 pessoa = Pessoa(*result)
-                return pessoa  
+                return pessoa
             else:
                 return None
-            
         except sqlite3.Error as e:
             print(f"Erro ao acessar SQLite: {e}")
             return None
         finally:
             cursor.close()
             conn.close()
-            
-class Conversor():
-    def __init__(self, usuario, data_conversao, moeda_entrada, moeda_saida, valor_entrada, valor_saida, cotacao, id=None):
+                
+class Conversao():
+    def __init__(self, usuario: str, data_conversao: str, moeda_entrada: str, moeda_saida: str, valor_entrada: float, valor_saida: float, cotacao: float, id: Optional[int] = None):
         self.id = id
         self.usuario = usuario
         self.data_conversao = data_conversao
@@ -76,41 +73,41 @@ class Conversor():
         self.valor_entrada = valor_entrada
         self.valor_saida = valor_saida
         self.cotacao = cotacao
-    def cadastrar(self):
-        conn = sqlite3.connect("conversor.db")
-        cursor = conn.cursor()
-        cursor.execute('''
-        INSERT INTO conversoes(usuario, data_conversao, moeda_entrada, moeda_saida, valor_entrada, valor_saida, cotacao)
-        VALUES(?, ?, ?, ?, ?, ?, ?)              
-        ''', (self.usuario, self.data_conversao, self.moeda_entrada, self.moeda_saida, self.valor_entrada, self.valor_saida, self.cotacao))
-        conn.commit()
-        cursor.close()
-        conn.close()
-        
-    def editar(self):
-        conn = sqlite3.connect("conversor.db")
-        cursor = conn.cursor()
-        if self.id is None:
-            raise ValueError("ID do usuário não especificado.")
-        cursor.execute('''
-        UPDATE conversoes usuario = ?, data_conversao = ?, moeda_entrada = ?, moeda_saida = ?, valor_entrada = ?, valor_saida = ?, cotacao = ?
-        WHERE id = ?              
-        ''',(self.usuario, self.data_conversao, self.moeda_entrada, self.moeda_saida, self.valor_entrada, self.valor_saida, self.cotacao, self.id))
-        conn.commit()
-        cursor.close()
-        conn.close()
-        
+    
+class ConversaoRepository:
 
-    def excluir(self):
-        conn = sqlite3.connect("conversor.db")
+    def cadastrar(self, conversor:Conversao) -> None:
+        conn = sqlite3.connect('conversor.db')
         cursor = conn.cursor()
-        if self.id is None:
+        cursor.execute("""
+        INSERT INTO conversoes (usuario, data_conversao, moeda_entrada, moeda_saida, valor_entrada, valor_saida, cotacao)
+        VALUES (?, ?, ?, ?, ?, ?, ?)             
+        """, (conversor.usuario, conversor.data_conversao, conversor.moeda_entrada, conversor.moeda_saida, conversor.valor_entrada, conversor.valor_saida, conversor.cotacao))
+        conn.commit()
+        
+    
+    def editar(self, conversao: Conversao) -> None:
+        if conversao.id is None:
             raise ValueError("ID do usuário não especificado.")
-        cursor.execute('DELETE FROM usuarios WHERE id = ?', (self.id))
+        conn = sqlite3.connect('conversor.db')
+        cursor = conn.cursor()
+        cursor.execute("""
+        UPDATE conversoes
+        SET usuario = ?, data_conversao = ?, moeda_entrada = ?, moeda_saida = ?, valor_entrada = ?, valor_saida = ?, cotacao = ?
+        WHERE id = ?                           
+        """,(conversao.usuario, conversao.data_conversao, conversao.moeda_entrada, conversao.moeda_saida, conversao.valor_entrada, conversao.valor_saida, conversao.cotacao, conversao.id))
         conn.commit()
         cursor.close()
         conn.close()
-        
-
+    
+    def excluir(self, conversao: Conversao) -> None:
+        if conversao.id is None:
+            raise ValueError("ID do usuário não especificado.")
+        conn = sqlite3.connect('conversor.db')
+        cursor = conn.cursor()
+        cursor.execute('DELETE FROM conversoes WHERE id = ?', (conversao.id,))
+        conn.commit()
+        cursor.close()
+        conn.close()
 
 
